830918c3db65027aae1e3ce6757e067c10439900
Lakshay696 <100407407+Lakshay696@users.noreply.github.com>
2024-07-14 10:42:42 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://lakshaybansal44:lakshay3048@cluster0.xetni5u.mongodb.net/food-delivery').then(()=>console.log("DB Connected"));
+    await mongoose.connect('').then(()=>console.log("DB Connected"));
 
 }
